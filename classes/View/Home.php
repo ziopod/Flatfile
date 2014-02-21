978f6e427721e44c2a9a3109a62c56efa358fba5
ziopod <ziopod@gmail.com>
2014-02-21 17:58:20 +0100

    Adding defautl home view

---
+++ classes/View/Home.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+* You can replace this class with your own stuffs
+*
+* @package		Flatfile
+* @category		View Model
+* @author		Ziopod <ziopod@gmail.com>
+* @copyright	(c) 2013-2014 Ziopod
+* @license		http://opensource.org/licenses/MIT
+**/
+
+class View_Home extends View_App{
+
+	public function content()
+	{
+		return 'This is home Mustache template, you can remplace it like you want.';
+	}
+}
