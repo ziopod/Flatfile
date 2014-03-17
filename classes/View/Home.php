f1635dba538e925bd6dd2f9c23ef9187809f10fb
ziopod <ziopod@gmail.com>
2014-03-17 20:38:00 +0100

    Better example for default home view

--- classes/View/Home.php
+++ classes/View/Home.php
@@ -3,6 +3,24 @@
 /**
 * You can replace this class with your own stuffs
 *
+* For example, you can aggregate FlatFile :
+*
+*	public function aside()
+*	{
+*		$flatfile_slug = 'asides/my_cool_aside';
+*
+*		try
+*		{
+*			return new Model_Page($flatfile_slug);
+*		}	
+*		catch(Kohana_Exception $e)
+*		{
+*			Log::instance()->add(Log::WARNING, 'Unable to find file ":slug.md", please check your "content" directory', array(
+*					':slug' => $flatfile_slug,
+*			));
+*		}
+*	}
+*
 * @package		Flatfile
 * @category		View Model
 * @author		Ziopod <ziopod@gmail.com>
@@ -10,10 +28,4 @@
 * @license		http://opensource.org/licenses/MIT
 **/
 
-class View_Home extends View_App{
-
-	public function content()
-	{
-		return 'This is home View and Template, you can remplace with your own stuff.';
-	}
-}
+class View_Home extends View_Page{}
