63139c8d2633be56fc536365af2ea200c9c03bf2
Ziopod <ziopod@gmail.com>
2015-03-26 12:22:11 +0100

    Removing config helper form init file

--- init.php
+++ init.php
@@ -1,50 +1,6 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 /**
-* Set environment status based on subdomain name or IP address
-**/
-$server_name = $_SERVER['SERVER_NAME'];
-
-if (strpos($server_name, 'dev.') !== FALSE OR $_SERVER['SERVER_ADDR'] == '127.0.0.1')
-{
-	Kohana::$environment = Kohana::DEVELOPMENT;
-}
-else if (strpos($server_name, 'test.') !== FALSE)
-{
-	Kohana::$environment = Kohana::TESTING;
-}
-else if (strpos($server_name, 'stage.') !== FALSE)
-{
-	Kohana::$environment = Kohana::STAGING;
-}
-else
-{
-	Kohana::$environment = Kohana::PRODUCTION;
-}
-
-/**
- * Attach a file reader to config. Multiple readers are supported.
- **/
-switch (Kohana::$environment) {
-	case Kohana::DEVELOPMENT:
-		Kohana::$config->attach(new Config_File('config/development'));
-		error_reporting(E_ALL);
-		break;
-	case Kohana::TESTING:
-		Kohana::$config->attach(new Config_File('config/testing'));
-		error_reporting(E_ALL ^ E_NOTICE ^ E_STRICT);
-		break;
-	case Kohana::STAGING:
-		Kohana::$config->attach(new Config_File('config/staging'));
-		error_reporting(E_ERROR);
-		break;
-	default:
-		Kohana::$config->attach(new Config_File());
-		error_reporting();
-		break;
-}
-
-/**
 * Autoload page example
 **/
 // Route::set('page', '<slug>', array(
